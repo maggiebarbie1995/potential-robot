704e8ba104976701cf2b78195fbee612afde2c75
maggiebarbie1995 <maggylangakush@gmail.com>
2020-06-24 22:08:03 +0300

    add final project

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
+
+import { FormsModule } from "@angular/forms";
+
+import {ProfileService} from './services/profile.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProfileComponent,
+     
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [ProfileService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
